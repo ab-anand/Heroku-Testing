38617d9a4951e539d8a89197cd80ce8843290594
ab-anand <abhinavanand1905@gmail.com>
2018-08-26 15:08:43 +0530

    version 1.0 done :D

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import (Flask, request, render_template,
                    session, flash, redirect, url_for)
+from info import projects, skills, tools
+
  
 app = Flask(__name__)
  
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', projects=projects, skills=skills, tools=tools)
 
 if __name__ == '__main__':
 	app.run(port=8000, debug=True)
