4282ccfa494abeac1f763120c25bb8dcd27c360f
ab-anand <abhinavanand1905@gmail.com>
2018-08-27 17:14:19 +0530

    modified and formated

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import (Flask, request, render_template,
                    session, flash, redirect, url_for)
 from info import projects, skills, tools
 
- 
+
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def index():
-	return render_template('index.html', projects=projects, skills=skills, tools=tools)
+    return render_template('index.html', projects=projects,
+                           skills=skills, tools=tools)
+
 
 if __name__ == '__main__':
-	app.run(port=8000, debug=True)
+    app.run(port=8000, debug=False)
--- info.py
+++ info.py
@@ -1,45 +1,33 @@
 projects = [
-			'Applying machine learning algos on multiple datasets',
-			'A speech recognition based digital assistant like Cortana',
-			'Python implementation of classic arcade game Space Invasion',
-			'An easy to use web implementation of Thesaurus using Flask',
-			'A desktop software to delete thousand of files in the blink of an eye',
-			'Various scrapping scripts in Python and JS',
-			'Automation Scripts with selenium and other tools',
-			'A social media like twitter in it\'s earlier days using Flask.'
-			]
+    'Applying machine learning algos on multiple datasets',
+    'A speech recognition based digital assistant like Cortana',
+    'Python implementation of classic arcade game Space Invasion',
+    'An easy to use web implementation of Thesaurus using Flask',
+    'A desktop software to delete thousand of files in the blink of an eye',
+    'Various scrapping scripts in Python and JS',
+    'Automation Scripts with selenium and other tools',
+    'A social media like twitter in it\'s earlier days using Flask.'
+]
 
 
 skills = [
-		'Machine Learning',
-		'Data mining & scrapping',
-		'Computer Vision & NLP',
-		'REST APIs design & implementation',
-		'Full stack Python Development'
-		]
+    'Machine Learning',
+    'Data mining & scrapping',
+    'Computer Vision & NLP',
+    'REST APIs design & implementation',
+    'Full stack Python Development'
+]
 
 
 tools = [
-		'tensorflow.png',
-		'mongo.png',
-		'opencv.png',
-		'flask.png',
-		'postgres.png',
-		'javascript.png',
-		'python.png',
-		'django.png',
-		'scikit.png',
-		'bootstrap.png'	
-		]
-
-abt_me = '''
-			I remember myself struggling through the concepts of C++ back in first year of college until one day
-			a senior of mine recommended me Python. From that day Python has became my major programming knowledge.
-
-			I remember wasting whole weeks over various Python projects & concepts instead of attending classes. I have 
-			tried to explore Python in all aspects, from using Asyncio to Keras.
-			Working with open-source made me familiar with some best coding practices and scaling applications. Open-source has 
-			been the biggest source of my knowledge so far, not just limited to tech.
-
-			When not coding, I can be found dancing, watching sci-fi shows, listening to pop songs. 
-			'''
+    'tensorflow.png',
+    'mongo.png',
+    'opencv.png',
+    'flask.png',
+    'postgres.png',
+    'javascript.png',
+    'python.png',
+    'django.png',
+    'scikit.png',
+    'bootstrap.png'
+]
