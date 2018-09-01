7bce732979b313ea375742a327eea55f7d784ed5
ab-anand <abhinavanand1905@gmail.com>
2018-09-01 11:49:11 +0530

    project details

--- info.py
+++ info.py
@@ -1,12 +1,12 @@
 projects = [
     ('A speech recognition based digital assistant written in Python like Cortana', 'Bruno'),
     ('Automation Scripts with selenium and other tools written in Python', 'Automation-Bots'),
-    ('Various scrapping scripts in Python and JS', 'Scrapping'),
+    ('Various scrapping scripts in Python and Cheerio JS', 'Scrapping'),
     ('Python implementation of classic arcade game Space Invasion', 'Spaceship'),
     ('A desktop software to delete thousand of files in the blink of an eye', 'AutoDel'),
     ('Pomodoro clock as desktop notifications written in Python', 'PyPomodoro'),
     ('An easy to use web implementation of Thesaurus using Flask', 'Vocab-Bird'),
-    ('Applying machine learning algos on multiple datasets', 'Data-Warehouse'),
+    ('Applying machine learning methods on datasets like Cancer, Wine, HR, etc.,', 'Data-Warehouse'), 
     ('A social media like twitter in it\'s earlier days using Flask', 'The-Social-Network'), 
     ('An image search engine using OpenCv', 'Image-Search-Engine')
 ]
